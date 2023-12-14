fe7ed1ea23f95daf68801175232a5a24b26fdcd4
ShriRam <sriramsingh289@gmail.com>
2023-12-14 19:01:00 +0530

    Concurrent Collections using CopyOnWriteArraylist

--- Tricky-Questions/src/concurrent_collections.java
+++
@@ -1,2 +0,0 @@
-public class concurrent_collections {
-}
---
+++ Tricky-Questions/src/list_taverse/concurrent_collections.java
@@ -0,0 +1,48 @@
+package list_taverse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class concurrent_collections {
+
+    public static void main(String[] args) {
+
+        List<String> list = new CopyOnWriteArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        list.add("fourth");
+        list.add("fifth");
+        list.add("sixth");
+        list.add("seven");
+        list.add("eight");
+        list.add("nine");
+        list.add("ten");
+
+        // thread 1
+        Runnable t = ()->{
+            for (String str : list){
+                System.out.println(list);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        // thread 2
+        Runnable t2 = ()->{
+            list.add("New item");
+            list.add("New item");
+            list.add("New item");
+        };
+
+        new Thread(t).start();
+        new Thread(t2).start();
+
+
+        System.out.println("\n Terminated Successfully");
+    }
+}
